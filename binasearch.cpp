2942e78309964cd2eb8f7b502c2544b4bae8a249
shaojwa <shaojwa@gmail.com>
2013-12-17 12:44:50 +0800

    refine getting mid

--- binasearch.cpp
+++ binasearch.cpp
@@ -4,27 +4,25 @@
 
 int binasearch(int *buf, int n, int tag);
 
-int buf[] = {1, 3, 7, 9, 13, 17, 19, 23, 29, 31, 37, 41, 53, 59};
+int buf[] = {1, 3, 7, 9, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59};
 
 int main(void)
 {
     int index = 0, hit, count = 0;
     int len = sizeof(buf) / sizeof(int);
-    long long llnum = strtoll();
-    printf("large = %llu\n", large);
-    printf("**** len = %d ****\n", len);
+    printf("**** len of buf = %d ****\n", len);
     for(index = 0; index < 60; index++)
     {
         hit = binasearch(buf, len, index);
         if (-1 != hit)
         {
-            printf("Yes: tag = %d, index = %d, buf[index] = %d\n",
+            printf("yes: tag = %d, index = %d, buf[index] = %d\n",
                 index, hit, buf[hit]);
             count++;
         }
         else
         {
-            printf("No: unfound: %d\n", index);
+            printf("no: unfound: %d\n", index);
         }
     }
     printf("**** count = %d ****\n", count);
@@ -32,7 +30,7 @@ int main(void)
 
 int binasearch(int *buf, int n, int tag)
 {
-    int left = 0, right = n - 1, mid = (left + right)/2;
+    int left = 0, right = n - 1, mid = left + ((right - left) >> 1);
     do
     {
         if (tag == buf[mid])
@@ -41,7 +39,7 @@ int binasearch(int *buf, int n, int tag)
             right = mid - 1;
         else 
             left = mid + 1;
-        mid = (left + right) / 2;
+        mid = left + ((right - left) >> 1);
     }while(left <= right);
     return -1;
 }
